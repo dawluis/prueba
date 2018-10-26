a1a67c141fb2fb45b36fc446447f235354b0e25a
Luis Marco <luismarco.web@gmail.com>
2018-10-26 17:27:48 +0200

    "form.php a medias"

--- UNIDAD1/expresionesRegulares/form.php
+++ UNIDAD1/expresionesRegulares/form.php
@@ -10,17 +10,47 @@ if(isset($_POST['enviar'])){
     
     if(empty($_POST['texto'])){
         $error=true;
-        $errores['texto']="EL TEXTO ESTA VACIO";
+        $errores['texto']="EL TEXTO ESTA VACIO POR FAVOR INTRODUCE COSITAS";
     }else{
+        $texto=$_POST['texto'];
+        $expresion=$_POST['seleccion'];
         
-        
-        
-        
+        switch($expresion){
+            case "codigopostal":
+                if(codigoPostal($texto)){
+                    $resultado="EL CODIGO POSTAL ES CORRECTO";
+                }else{
+                    $resultado="EL CODIGO POSTAL ES INCORRECTO";
+                }
+                
+                break;
+            
+            case "telefono":
+                if(numTelefono($texto)){
+                    $resultado="EL TELEFONO ES CORRECTO";
+                }else{
+                    $resultado="EL TELEFONO ES INCORRECTO";
+                    $error=true;
+                    $errores['telefono']=$resultado;
+                   
+                }
+              
+                break;
+                
+            case "direccion":
+                if(direccion($texto)){
+                    $resultado="LA CALLE ES CORRECTA";
+                }else{
+                    $resultado="LA CALLE ES INCORRECTA";
+                }
+                
+                break;
+        }
+        echo $resultado;
         
     }
     if($error){
         print_r($errores);
-        
         include 'formulario.php';
         
     }
--- UNIDAD1/expresionesRegulares/formulario.php
+++ UNIDAD1/expresionesRegulares/formulario.php
@@ -6,12 +6,12 @@
     <form action="" method="POST">
         <input type="text" name="texto">
         <select name="seleccion">
-            <option value="CodigoPostal">CodigoPostal</option>
-            <option value="CodigoPostal">Telefono</option>
-            <option value="CodigoPostal">Direccion</option>
-            <option value="CodigoPostal">Usuario</option>
-            <option value="CodigoPostal">Email</option>
-            <option value="CodigoPostal">otro</option>
+            <option value="codigopostal">CodigoPostal</option>
+            <option value="telefono">Telefono</option>
+            <option value="direccion">Direccion</option>
+            <option value="usuario">Usuario</option>
+            <option value="email">Email</option>
+            <option value="otro">otro</option>
         </select>
         
         
