fe6904fc252691e5a6490917bb9028ce6431b551
luis <luismarco.web@gmail.com>
2018-11-17 20:06:18 +0100

    fin

---
+++ UNIDAD1/ficheros/contador.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<style type="text/css">
+	</style>
+	</head>
+	<body>
+<?php
+include '1-ficheros.php';
+$visitas=contador("counter.txt");
+echo "<h2> LA PAGINA A TENIDO $visitas visitas ";
+?>
+</body>
+</html>
