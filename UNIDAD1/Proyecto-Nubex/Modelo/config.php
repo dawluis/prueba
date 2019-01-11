807e9ca9fcc07d9f16c626f9dec3eacd5ad1fc61
Luis Marco <luismarco.web@gmail.com>
2019-01-11 17:45:26 +0100

    nubex fin

--- UNIDAD1/Proyecto-Nubex/Modelo/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Config {
-    
-    public static $hostname="localhost";
-    public static $dbName="nubex";
-    public static $dbUserName="root";
-    public static $dbPassword="";
-}
-?>
